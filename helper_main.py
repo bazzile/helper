45d5a8e4cbf8ce3514e2b78af6ce7fd385f91db4
lobanov.office <bazzzzile@gmail.com>
2016-10-07 20:52:20 +0300

    Дополнен диалог импорта

--- helper_main.py
+++ helper_main.py
@@ -30,11 +30,23 @@ from helper_main_dialog import HelperDialog
 import os.path
 
 
-def set_sat_list():
-    """"Задаём список доступных спутников"""
-    sat_list = ["DEIMOS2", "BKA", "TRIPLESAT", "TH", "GF1", "ZY3", "GF2", "KAZEOSAT1", "KAZEOSAT2", "ALOS", "PRISM",
-                "DG/WV-QB-IK-GE", "SPOT5", "SPOT67", "KOMPSAT2", "KOMPSAT3", ]
-    return sat_list
+class Satellite:
+    def __init__(self):
+        self.satellite = None
+        """"Задаём список доступных спутников"""
+        self.sat_list = ["DEIMOS2", "BKA", "TRIPLESAT", "TH", "GF1", "ZY3", "GF2", "KAZEOSAT1", "KAZEOSAT2", "ALOS",
+                         "PRISM",
+                         "DG/WV-QB-IK-GE", "SPOT5", "SPOT67", "KOMPSAT2", "KOMPSAT3", ]
+
+    def get_sat_list(self):
+        return self.sat_list
+
+    def set_curr_sat(self, new_satellite_value):
+        self.satellite = new_satellite_value
+        return self.satellite
+
+    def get_curr_sat(self):
+        return self.satellite
 
 
 class Helper:
@@ -77,8 +89,9 @@ class Helper:
         self.toolbar.setObjectName(u'Helper')
 
         # мои переменные
-        self.filename = None
+        self.curr_filepath = None
         self.last_used_path = None
+        self.satellite = Satellite()
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -180,13 +193,13 @@ class Helper:
 
     def populateGui(self):
         """Make the GUI live."""
-        self.populateComboBox(self.dlg.SENSOR, set_sat_list(), u'...', True)
+        self.populateComboBox(self.dlg.SENSOR, self.satellite.get_sat_list(), u'Какой спутник ищем?', True)
+        self.dlg.INPUT.setText(u'Где взять исходные файлы?')
 
-        # при выборе спутника переключаем текущую [currentIndex()] вкладку на следующую
-        self.dlg.SENSOR.activated.connect(
-            lambda: self.dlg.toolBox.setCurrentIndex(int(self.dlg.toolBox.currentIndex()) + 1))
-        self.dlg.INPUT.setText(u'Хуй')
-        self.dlg.INPUTbrowse.clicked.connect(self.select_input_file)
+    def runGui(self):
+        self.dlg.SENSOR.currentIndexChanged.connect(lambda: self.satellite.set_curr_sat(self.dlg.SENSOR.currentText()))
+        self.dlg.INPUTbrowse.clicked.connect(
+            lambda: self.select_input_file(sensor=self.satellite.get_curr_sat()))
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
@@ -201,6 +214,7 @@ class Helper:
     def run(self):
         """Run method that performs all the real work"""
         self.populateGui()
+        self.runGui()
         # show the dialog
         self.dlg.show()
         # Run the dialog event loop
@@ -236,16 +250,25 @@ class Helper:
                 combo.setCurrentIndex(0)
         combo.blockSignals(False)
 
-    def select_input_file(self):
-        if self.last_used_path is None:
-            self.filename = QFileDialog.getOpenFileName(
-                self.dlg, u"Укажите файл контура ", "", u'Полигоны (*.shp *.kml *tab *geojson)')
-            self.dlg.INPUT.setText(self.filename)
-            # записываем в self.last_used_path последний использовавшийся каталог
-            self.last_used_path = os.path.dirname(self.filename)
+    def select_input_file(self, sensor):
+        if sensor == 'BKA':
+            file_format = u' БКА (*.kml *.kmz *.KML *.KMZ)'
         else:
-            # TODO заменить это на очищение поля (если выбран другой спутник), но оставить last_used_path
-            self.dlg.INPUT.setText(self.filename)
+            file_format = u'??? (Сенсор не задан)'
+        if self.last_used_path is None:
+            self.curr_filepath = QFileDialog.getOpenFileName(
+                self.dlg, u"Укажите файл контура ", "c:\\", file_format)
+            # записываем в self.last_used_path последний использовавшийся каталог
+            self.dlg.INPUT.setText(self.curr_filepath)
+        elif self.last_used_path is not None and self.curr_filepath is None:
+            self.last_used_path = "c:\\"
+        else:
+            self.curr_filepath = QFileDialog.getOpenFileName(
+                self.dlg, u"Укажите файл контура ", self.last_used_path, file_format)
+            self.last_used_path = os.path.dirname(self.curr_filepath)
+            self.dlg.INPUT.setText(self.curr_filepath)
+        if self.curr_filepath is None:
+            self.dlg.INPUT.setText(u'Где взять исходные файлы?')
 
     # TODO сделать select_..._ функцией по типу populate_combo
     def select_output_dir(self):
---
+++ ql_exporter.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import shutil
+import xml.etree.ElementTree as ET
+from string import Template
+from PIL import Image
+import zipfile
+
+
+def bka_ql_exporter(source_file, dst_dirpath):
+    src_file = source_file
+    # TODO название директории dst_dir_name/path должно выбираться из Helper
+    dst_dir_name = 'QuickLooks'
+    dst_dir_path = os.path.join(dst_dirpath, dst_dir_name)
+
+    if not os.path.exists(dst_dir_path):
+        os.makedirs(dst_dir_path)
+
+    # парсим kml
+    if src_file.endswith(('.kml', '.KML')):
+        tree = ET.parse(src_file)
+    else:
+        # src_file.endswith(('.kmz', '.KMZ'))
+        with zipfile.ZipFile(src_file) as kmz:
+            for filename in kmz.namelist():
+                if filename.endswith(('.kml', '.KML')):
+                    # парсим kml
+                    # TODO проверить, закрывается ли kml просле окончания with
+                    with kmz.open(filename, 'r') as kml:
+                        tree = ET.parse(kml)
+                    break
+    root = tree.getroot()
+
+    # ищем в kml все записи, описывающие квиклук
+    ql_kml_list = root.findall(".//GroundOverlay")
+
+    # print(len(q))
+    for q in range(len(ql_kml_list)):
+        ql_filename = ql_kml_list[q].find(".//href").text
+        standard_ql_name = ql_filename[:13]
+        # стандартизируем имя и копируем квиклук в целевую директорию, где измеряем его пикс. ширину и высоту
+        ql_dst_path = os.path.join(dst_dir_path, standard_ql_name + '.jpg')
+        if src_file.endswith(('.kml', '.KML')):
+            shutil.copy(os.path.join(dst_dir_path, ql_filename), ql_dst_path)
+        else:
+            with zipfile.ZipFile(src_file) as kmz:
+                with kmz.open(ql_filename) as zipped_ql, open(ql_dst_path, 'wb') as f:
+                    shutil.copyfileobj(zipped_ql, f)
+        ql_image_obj = Image.open(ql_dst_path)
+        ql_width, ql_height = ql_image_obj.size[0], ql_image_obj.size[1]
+        coords_str = ql_kml_list[q].find(".//coordinates").text
+        # преобразуем строку с координатами углов в список и разбиваем по 4 точкам
+        coords_lst = coords_str.split('\n')
+        c1, c2, c3, c4 = coords_lst[3], coords_lst[0], coords_lst[1], coords_lst[2]
+        text_content = Template('!table\n'
+                                '!version 300\n'
+                                '!charset WindowsCyrillic\n'
+                                'Definition Table\n'
+                                '  File "$file_name"\n'
+                                '  Type "RASTER"\n'
+                                '  ($map_coords1)  (0.0,0.0) Label "Point 1",\n'
+                                '  ($map_coords2)  (0.0,$img_hight.0) Label "Point 2",\n'
+                                '  ($map_coords3)  ($img_width.0,$img_hight.0) Label "Point 3",\n'
+                                '  ($map_coords4)  ($img_width.0,0.0) Label "Point 4"\n'
+                                ' CoordSys Earth Projection 1, 0\n')
+        text_content = text_content.substitute(
+            file_name=standard_ql_name + '.jpg', map_coords1=c1, map_coords2=c2, map_coords3=c3, map_coords4=c4,
+            img_hight=str(ql_height), img_width=str(ql_width))
+
+        with open(os.path.join(dst_dir_path, standard_ql_name + '.tab'), 'w') as f:
+            f.write(text_content.strip())
