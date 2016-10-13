17b0e660c9976387013d6abc7dffc42d915c1e06
lobanov.office <bazzzzile@gmail.com>
2016-10-13 14:22:06 +0300

    TH правильный поворот

--- temp_TH.py
+++ temp_TH.py
@@ -10,13 +10,21 @@ import zipfile
 import contextlib
 import tempfile
 import ogr
+from ql_exporter import tab_template
 
 
 # TODO убрать это
 dir = os.path.dirname(__file__)
 src_path = os.path.join(dir, r"testData\TH\185205596")
+dst_dir_path = os.path.join(dir, src_path)
 
 src_file =r"C:\Users\lobanov\.qgis2\python\plugins\Helper\testData\TH\185205596\185205596.shp"
+
+
+def get_ql_path(qiucklook_name):
+    for ql_path in ql_path_list:
+        if qiucklook_name == ql_path.split('.')[-2][-46:-4]:
+            return ql_path
 
 ql_path_list = []
 for dirpath, dirnames, filenames in os.walk(os.path.join(dir, src_path)):
@@ -32,16 +40,21 @@ dataSource = driver.Open(src_file, 0)
 layer = dataSource.GetLayer(0)
 for img_contour in layer:
     ql_name = img_contour.GetField('ImgIdDgp')
-    print(ql_name)
     geometry = img_contour.GetGeometryRef()
     ring = geometry.GetGeometryRef(0)
     numpoints = ring.GetPointCount()
+    coord_list = ['', '', '', '']
+    list_counter = 0
     for point_id in range(ring.GetPointCount() - 1):
         lon, lat, z = ring.GetPoint(point_id)
-        print (','.join((str(lat), str(lon))))
+        coord_list[list_counter] = str(','.join((str(lon), str(lat))))
+        list_counter += 1
     print('\n')
+    ql_path = get_ql_path(ql_name)
+    ql_image_obj = Image.open(ql_path)
+    ql_width, ql_height = ql_image_obj.size[0], ql_image_obj.size[1]
+    text_content = tab_template(
+        'TH', ql_name + '_Bro', coord_list[0], coord_list[3], coord_list[2], coord_list[1], ql_height, ql_width)
+    with open(os.path.join(dst_dir_path, ql_name + '_Bro' + '.tab'), 'w') as f:
+        f.write(text_content.strip())
 dataSource = None
-
-for ql_path in ql_path_list:
-    print(ql_path)
-    print ql_path.split('.')[-2][-46:-22]
