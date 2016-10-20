38f1faddc2b36ff926b0ade7f354aaa408b1467b
lobanov.office <bazzzzile@gmail.com>
2016-10-20 16:28:16 +0300

    рабочая версия с BKA

--- ql_exporter.py
+++ ql_exporter.py
@@ -9,8 +9,6 @@ from StringIO import StringIO
 import zipfile
 import urllib
 from osgeo import ogr
-import contextlib
-import tempfile
 import auxiliary_functions
 
 
@@ -49,6 +47,52 @@ def tab_template(sensor, file_name, map_coords1, map_coords2, map_coords3, map_c
     return text_content.substitute(
         file_name=file_name.split('.')[0] + '.jpg', map_coords1=map_coords1, map_coords2=map_coords2,
         map_coords3=map_coords3, map_coords4=map_coords4, img_hight=str(img_hight), img_width=str(img_width))
+
+
+def bka_ql_exporter(source_file, dst_dirpath):
+    # парсим kml
+    if source_file.endswith(('.kml', '.KML')):
+        tree = ET.parse(source_file)
+    else:
+        # src_file.endswith(('.kmz', '.KMZ'))
+        with zipfile.ZipFile(source_file) as kmz:
+            for filename in kmz.namelist():
+                if filename.endswith(('.kml', '.KML')):
+                    # парсим kml
+                    # TODO проверить, закрывается ли kml просле окончания with
+                    with kmz.open(filename, 'r') as kml:
+                        tree = ET.parse(kml)
+                    break
+
+    root = tree.getroot()
+    # ищем в kml все записи, описывающие квиклук
+    ql_kml_list = root.findall(".//GroundOverlay")
+    counter = 0
+    for q in range(len(ql_kml_list)):
+        ql_filename = ql_kml_list[q].find(".//href").text
+        standard_ql_name = ql_filename[:13]
+        # стандартизируем имя и копируем квиклук в целевую директорию, где измеряем его пикс. ширину и высоту
+        ql_dst_path = os.path.join(dst_dirpath, standard_ql_name + '.jpg')
+        if source_file.endswith(('.kml', '.KML')):
+            shutil.copy(os.path.join(os.path.dirname(source_file), ql_filename), ql_dst_path)
+        else:
+            with zipfile.ZipFile(source_file) as kmz:
+                with kmz.open(ql_filename) as zipped_ql, open(ql_dst_path, 'wb') as f:
+                    shutil.copyfileobj(zipped_ql, f)
+        ql_image_obj = Image.open(ql_dst_path)
+        ql_width, ql_height = ql_image_obj.size[0], ql_image_obj.size[1]
+        del ql_image_obj
+        coords_str = ql_kml_list[q].find(".//coordinates").text
+        # преобразуем строку с координатами углов в список и разбиваем по 4 точкам
+        coords_lst = coords_str.split('\n')
+        c1, c2, c3, c4 = coords_lst[3], coords_lst[0], coords_lst[1], coords_lst[2]
+        text_content = tab_template('bka', standard_ql_name, c1, c2, c3, c4, ql_height, ql_width)
+        with open(os.path.join(dst_dirpath, standard_ql_name + '.tab'), 'w') as f:
+            f.write(text_content.strip())
+        counter += 1
+        percent_done = 100 * counter / len(ql_kml_list)
+        # этот callback позволяет отслеживать прогресс функции в helper_main
+        yield percent_done, len(ql_kml_list)
 
 
 def deimos_ql_exporter(source_file, dst_dirpath):
@@ -99,75 +143,6 @@ def deimos_ql_exporter(source_file, dst_dirpath):
                         percent_done = 100 * counter / len(ql_list)
                         # этот callback позволяет отслеживать прогресс функции в helper_main
                         yield percent_done, len(ql_list)
-
-
-def bka_ql_exporter(source_file, dst_dirpath, open_on_finish=True):
-    src_file = source_file
-    # TODO название директории dst_dir_name/path должно выбираться из Helper
-    dst_dir_name = 'QuickLooks'
-    dst_dir_path = os.path.join(dst_dirpath, dst_dir_name)
-
-    if not os.path.exists(dst_dir_path):
-        os.makedirs(dst_dir_path)
-
-    # парсим kml
-    if src_file.endswith(('.kml', '.KML')):
-        tree = ET.parse(src_file)
-    else:
-        # src_file.endswith(('.kmz', '.KMZ'))
-        with zipfile.ZipFile(src_file) as kmz:
-            for filename in kmz.namelist():
-                if filename.endswith(('.kml', '.KML')):
-                    # парсим kml
-                    # TODO проверить, закрывается ли kml просле окончания with
-                    with kmz.open(filename, 'r') as kml:
-                        tree = ET.parse(kml)
-                    break
-    root = tree.getroot()
-
-    # ищем в kml все записи, описывающие квиклук
-    ql_kml_list = root.findall(".//GroundOverlay")
-
-    # print(len(q))
-    for q in range(len(ql_kml_list)):
-        ql_filename = ql_kml_list[q].find(".//href").text
-        standard_ql_name = ql_filename[:13]
-        # стандартизируем имя и копируем квиклук в целевую директорию, где измеряем его пикс. ширину и высоту
-        ql_dst_path = os.path.join(dst_dir_path, standard_ql_name + '.jpg')
-        if src_file.endswith(('.kml', '.KML')):
-            shutil.copy(os.path.join(os.path.dirname(src_file), ql_filename), ql_dst_path)
-        else:
-            with zipfile.ZipFile(src_file) as kmz:
-                with kmz.open(ql_filename) as zipped_ql, open(ql_dst_path, 'wb') as f:
-                    shutil.copyfileobj(zipped_ql, f)
-        ql_image_obj = Image.open(ql_dst_path)
-        ql_width, ql_height = ql_image_obj.size[0], ql_image_obj.size[1]
-        coords_str = ql_kml_list[q].find(".//coordinates").text
-        # преобразуем строку с координатами углов в список и разбиваем по 4 точкам
-        coords_lst = coords_str.split('\n')
-        c1, c2, c3, c4 = coords_lst[3], coords_lst[0], coords_lst[1], coords_lst[2]
-        text_content = Template('!table\n'
-                                '!version 300\n'
-                                '!charset WindowsCyrillic\n'
-                                'Definition Table\n'
-                                '  File "$file_name"\n'
-                                '  Type "RASTER"\n'
-                                '  ($map_coords1)  (0.0,0.0) Label "Point 1",\n'
-                                '  ($map_coords2)  (0.0,$img_hight.0) Label "Point 2",\n'
-                                '  ($map_coords3)  ($img_width.0,$img_hight.0) Label "Point 3",\n'
-                                '  ($map_coords4)  ($img_width.0,0.0) Label "Point 4"\n'
-                                ' CoordSys Earth Projection 1, 0\n')
-        text_content = text_content.substitute(
-            file_name=standard_ql_name + '.jpg', map_coords1=c1, map_coords2=c2, map_coords3=c3, map_coords4=c4,
-            img_hight=str(ql_height), img_width=str(ql_width))
-
-        with open(os.path.join(dst_dir_path, standard_ql_name + '.tab'), 'w') as f:
-            f.write(text_content.strip())
-    # dlg.progressBar.setValue(50)
-    if open_on_finish is True:
-        os.startfile(dst_dir_path)
-    else:
-        pass
 
 
 def th_ql_exporter(source_file, dst_dirpath):
