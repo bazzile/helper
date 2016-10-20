96e0fd51d3121ab63554ec60fe7f7784767fe6b4
lobanov.office <bazzzzile@gmail.com>
2016-10-20 16:16:52 +0300

    рабочая версия DEIMOS

--- ql_exporter.py
+++ ql_exporter.py
@@ -7,7 +7,7 @@ from string import Template
 from PIL import Image
 from StringIO import StringIO
 import zipfile
-# import requests
+import urllib
 from osgeo import ogr
 import contextlib
 import tempfile
@@ -51,63 +51,54 @@ def tab_template(sensor, file_name, map_coords1, map_coords2, map_coords3, map_c
         map_coords3=map_coords3, map_coords4=map_coords4, img_hight=str(img_hight), img_width=str(img_width))
 
 
-def deimos_ql_exporter(source_file, dst_dirpath, open_on_finish=True):
-    @contextlib.contextmanager
-    def make_temp_directory():
-        temp_dir = tempfile.mkdtemp()
-        try:
-            yield temp_dir
-        finally:
-            shutil.rmtree(temp_dir)
-    # TODO следующий блок дублируется в БКА.
-    dst_dir_name = 'QuickLooks'
-    dst_dir_path = os.path.join(dst_dirpath, dst_dir_name)
-    if not os.path.exists(dst_dir_path):
-        os.makedirs(dst_dir_path)
-    with make_temp_directory() as tmpdir:
+def deimos_ql_exporter(source_file, dst_dirpath):
+    with auxiliary_functions.make_temp_directory() as tmpdir:
         with zipfile.ZipFile(source_file, 'r') as zfile:
             zfile.extractall(tmpdir)
-        ql_list = len([name for name in os.listdir(r"C:\NextGIS_QGIS\apps\qgis\python\plugins\Helper\testData\BKA\QuickLooks") if
-             name.endswith(('.kmz', '.KMZ')) and name[-7:-4] != 'ALL'])
+            ql_list = [f for dp, dn, filenames in os.walk(tmpdir) for f in filenames if f.endswith(('.kmz', '.KMZ'))
+                       and f[-7:-4] != 'ALL']
         for dirpath, dirnames, filenames in os.walk(tmpdir):
+            counter = 0
             for filename in filenames:
                 if filename.endswith(('.kmz', '.KMZ')) and filename[-7:-4] != 'ALL':
                     in_file = os.path.join(dirpath, filename)
                     with zipfile.ZipFile(in_file, 'r') as kmz:
                         with kmz.open('doc.kml', 'r') as kml:
                             kml_tree = ET.parse(kml)
-                        # kml_file = kmz.open('doc.kml', 'r')
-                    # kmz = zipfile.ZipFile(in_file, 'r')
-                    # kml_file = kmz.open('doc.kml', 'r')
-                    # kml_tree = ET.parse(kml_file)
                     root = kml_tree.getroot()
                     ql_kml_list = root.findall(".//{http://earth.google.com/kml/2.1}GroundOverlay")
-                    counter = 0
                     for q in range(len(ql_kml_list)):
                         ql_filename = ql_kml_list[q].find(".//{http://earth.google.com/kml/2.1}name").text
                         standard_ql_name = ql_filename[11:]
                         ql_dst_path = os.path.join(dst_dirpath, standard_ql_name + '.jpg')
                         ql_url = ql_kml_list[q].find(".//{http://earth.google.com/kml/2.1}href").text
-                        response = requests.get(ql_url)
-                        if response.status_code == 200:
-                            i = Image.open(StringIO(response.content))
-                            i.save(ql_dst_path, format='JPEG', subsampling=0, quality=100)
-                        else:
-                            # TODO выводить ошибку "не удалось скачать квиклук с сервера DEIMOS
-                            raise IOError
+                        content = StringIO(urllib.urlopen(ql_url).read())
+                        i = Image.open(content)
+                        i.save(ql_dst_path, format='JPEG', quality=80)
+                        del i
+                        # response = requests.get(ql_url)
+                        # if response.status_code == 200:
+                        #     i = Image.open(StringIO(response.content))
+                        #     i.save(ql_dst_path, format='JPEG', quality=80)
+                        # else:
+                        #     # TODO выводить ошибку "не удалось скачать квиклук с сервера DEIMOS
+                        #     raise IOError
                         ql_image_obj = Image.open(ql_dst_path)
                         ql_width, ql_height = ql_image_obj.size[0], ql_image_obj.size[1]
+                        del ql_image_obj
                         north = ql_kml_list[q].find(".//{http://earth.google.com/kml/2.1}north").text
                         south = ql_kml_list[q].find(".//{http://earth.google.com/kml/2.1}south").text
                         east = ql_kml_list[q].find(".//{http://earth.google.com/kml/2.1}east").text
                         west = ql_kml_list[q].find(".//{http://earth.google.com/kml/2.1}west").text
                         c1, c2, c3, c4 = ','.join((str(west), str(north))), ','.join((str(east), str(north))), \
                                          ','.join((str(east), str(south))), ','.join((str(west), str(south)))
-                        print c1, c2, c3, c4
                         text_content = tab_template('deimos', standard_ql_name, c1, c2, c3, c4, ql_height, ql_width)
-                        with open(os.path.join(dst_dir_path, standard_ql_name + '.tab'), 'w') as f:
+                        with open(os.path.join(dst_dirpath, standard_ql_name + '.tab'), 'w') as f:
                             f.write(text_content.strip())
-                break
+                        counter += 1
+                        percent_done = 100 * counter / len(ql_list)
+                        # этот callback позволяет отслеживать прогресс функции в helper_main
+                        yield percent_done, len(ql_list)
 
 
 def bka_ql_exporter(source_file, dst_dirpath, open_on_finish=True):
