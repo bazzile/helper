90579753da4303e0ae51cb1230cf57abf2ba6f5a
home_ASUS <bazzzzile@gmail.com>
2016-10-24 21:02:26 +0300

    исправлена табуляция

--- my_ellipsoidal_area.py
+++ my_ellipsoidal_area.py
@@ -15,22 +15,22 @@ from qgis.core import *
 
 def ellipsoidal_area(input_lyr, ellipsoid, new_field, units, output, progress):
     measure_units_dict = {0: 'sq_km', 1: 'sq_m', 2: 'sq_miles', 3: 'sq_ft',
-            4: 'sq_nm', 5:'sq_degrees'}
+                          4: 'sq_nm', 5: 'sq_degrees'}
     units_selection = measure_units_dict[units]
     input_layer = QgsMapLayerRegistry.instance().mapLayersByName(input_lyr)[0]
     if not input_layer.crs().geographicFlag():
-      raise GeoAlgorithmExecutionException(
-          'Your layer has a Projected CRS. '
-          'This script works only on layers with Geographic CRS.')
+        raise GeoAlgorithmExecutionException(
+            'Your layer has a Projected CRS. '
+            'This script works only on layers with Geographic CRS.')
 
     fields = QgsFields()
     for field in input_layer.pendingFields():
-      if field.name().lower() == new_field.lower():
-        raise GeoAlgorithmExecutionException(
-            'The input layer already has a field named %s.'
-            'Please choose a different name for the Area field.' % new_field)
+        if field.name().lower() == new_field.lower():
+            raise GeoAlgorithmExecutionException(
+                'The input layer already has a field named %s.'
+                'Please choose a different name for the Area field.' % new_field)
 
-      fields.append(field)
+        fields.append(field)
     fields.append(QgsField(new_field, QVariant.Double))
 
     writer = VectorWriter(output, None, fields,
@@ -48,40 +48,40 @@ def ellipsoidal_area(input_lyr, ellipsoid, new_field, units, output, progress):
     num_features = len(features)
 
     for i, feat in enumerate(features):
-      progress.setValue(int(100 *i / num_features))
-      geom = feat.geometry()
-      polygon_area = 0
-      if geom.isMultipart():
-        polygons = geom.asMultiPolygon()
-        for polygon in polygons:
-          polygon_area += area.measurePolygon(polygon[0])
-      else:
-        polygon = geom.asPolygon()
-        polygon_area = area.measurePolygon(polygon[0])
+        progress.setValue(int(100 * i / num_features))
+        geom = feat.geometry()
+        polygon_area = 0
+        if geom.isMultipart():
+            polygons = geom.asMultiPolygon()
+            for polygon in polygons:
+                polygon_area += area.measurePolygon(polygon[0])
+        else:
+            polygon = geom.asPolygon()
+            polygon_area = area.measurePolygon(polygon[0])
 
-      # calculated area is in sq. metres
-      if units_selection == 'sq_km':
-        final_area = polygon_area / 1e6
-      elif units_selection == 'sq_ft':
-        final_area = area.convertMeasurement(
-            polygon_area, QGis.Meters, QGis.Feet, True)[0]
-      elif units_selection == 'sq_miles':
-        final_area = area.convertMeasurement(
-            polygon_area, QGis.Meters, QGis.Feet, True)[0] / (5280.0 * 5280.0)
-      elif units_selection == 'sq_nm':
-        final_area = area.convertMeasurement(
-            polygon_area, QGis.Meters, QGis.NauticalMiles, True)[0]
-      elif units_selection == 'sq_degrees':
-        final_area = area.convertMeasurement(
-            polygon_area, QGis.Meters, QGis.Degrees, True)[0]
-      else:
-        final_area = polygon_area
+        # calculated area is in sq. metres
+        if units_selection == 'sq_km':
+            final_area = polygon_area / 1e6
+        elif units_selection == 'sq_ft':
+            final_area = area.convertMeasurement(
+                polygon_area, QGis.Meters, QGis.Feet, True)[0]
+        elif units_selection == 'sq_miles':
+            final_area = area.convertMeasurement(
+                polygon_area, QGis.Meters, QGis.Feet, True)[0] / (5280.0 * 5280.0)
+        elif units_selection == 'sq_nm':
+            final_area = area.convertMeasurement(
+                polygon_area, QGis.Meters, QGis.NauticalMiles, True)[0]
+        elif units_selection == 'sq_degrees':
+            final_area = area.convertMeasurement(
+                polygon_area, QGis.Meters, QGis.Degrees, True)[0]
+        else:
+            final_area = polygon_area
 
-      attrs = feat.attributes()
-      attrs.append(final_area)
-      out_f.setGeometry(geom)
-      out_f.setAttributes(attrs)
-      writer.addFeature(out_f)
+        attrs = feat.attributes()
+        attrs.append(final_area)
+        out_f.setGeometry(geom)
+        out_f.setAttributes(attrs)
+        writer.addFeature(out_f)
 
     progress.setValue(100)
     del writer
