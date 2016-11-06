40318d432ca4b176438e479e258f967abec4fa5a
lobanovLenovoOFFICE <lobanov@innoter.com>
2016-11-07 00:19:42 +0300

    remove_xml_namespace

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -12,12 +12,10 @@ from qgis.core import *
 
 
 def make_out_dir(dst_dirpath):
-    """Функия берет путь и создаёт в нём каталог QuickLooks, возвращая его путь dst_dir_path"""
-    dst_dir_name = 'QuickLooks'
-    dst_dir_path = os.path.join(dst_dirpath, dst_dir_name)
-    if not os.path.exists(dst_dir_path):
-        os.makedirs(dst_dir_path)
-    return dst_dir_path
+    """Функия берет путь и создаёт эту папку (если она не существует), возвращая этот же путь в dst_dirpath"""
+    if not os.path.exists(dst_dirpath):
+        os.makedirs(dst_dirpath)
+    return dst_dirpath
 
 
 @contextlib.contextmanager
